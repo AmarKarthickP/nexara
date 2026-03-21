ab548bd8f40e710766b460a34ca88cc50612c818
Amar Karthick P <karthickpamar@gmail.com>
2026-03-21 20:15:36 +0530

    Doctype setup

---
+++ nexara/nexara_ai/doctype/nexara_page/nexara_page.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Amar Karthick P and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class NexaraPage(Document):
+	pass
---
+++ nexara/nexara_ai/doctype/nexara_page_data/nexara_page_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Amar Karthick P and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Nexara Page Data", {
+// 	refresh(frm) {
+
+// 	},
+// });
